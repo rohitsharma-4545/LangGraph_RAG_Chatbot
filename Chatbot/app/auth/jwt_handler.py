8fd10bca78f39515134aa546022cc5e2e4f751f2
JesTech <temovo2434@roratu.com>
2026-04-30 15:07:40 +0530

    Minor fix

--- Chatbot/app/auth/jwt_handler.py
+++ Chatbot/app/auth/jwt_handler.py
@@ -1,15 +1,17 @@
+import os
 from jose import jwt
 from datetime import datetime, timedelta
 
-SECRET_KEY = "supersecret"
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+EXP_HOURS = int(os.getenv("JWT_EXP_HOURS", 10))
 
 
 def create_token(user_id: str, role: str = "user"):
     payload = {
         "sub": user_id,
         "role": role,
-        "exp": datetime.utcnow() + timedelta(hours=10)
+        "exp": datetime.utcnow() + timedelta(hours=EXP_HOURS)
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
